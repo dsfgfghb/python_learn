b8d1c24a39b3bb34ca6c2f3e196ec4ff790b8ce6
fjy <q13467900@outlook.com>
2024-05-08 20:32:51 +0800

    1

---
+++ 1.1.py
@@ -0,0 +1,12 @@
+# print("hello world")
+
+message="hello python world"
+print(message)
+
+message="Hello Python Crash Course world!"
+print(message)
+
+name = "ada lovelace"
+print(name.title())  #首字母大写
+print(name.upper())  #所有字母大写
+print(name.lower())  #所有字母小写
