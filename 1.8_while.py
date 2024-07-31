1064386ebd40faec02f1c722d20e54e6c92a51a9
noob <q13467900@outlook.com>
2024-08-01 00:03:25 +0800

    while

--- 1.8_while.py
+++ 1.8_while.py
@@ -22,12 +22,12 @@ print("7.1.2 使用 int() 来获取数值输入")
 
 # age  = int(age)                     #将字符串转化成int类型
 
-height = input("How tall are you, in inches? ")
-height = int(height)
-if height >= 48:                            #转换成int类型之后可以进行数值比较
-    print("\nYou're tall enough to ride!")
-else:
-    print("\nYou'll be able to ride when you're a little older.")
+# height = input("How tall are you, in inches? ")
+# height = int(height)
+# if height >= 48:                            #转换成int类型之后可以进行数值比较
+#     print("\nYou're tall enough to ride!")
+# else:
+#     print("\nYou'll be able to ride when you're a little older.")
     
     
 print("----------------------------------------------------------------------------------------")
@@ -36,3 +36,66 @@ print("7.1.3 求模运算符 % ")
 
 #  %  为取余数
 
+
+print("----------------------------------------------------------------------------------------")
+#7.2.1 使用 while 循环
+print("7.2.1 使用 while 循环")
+current_nomber = 1
+while current_nomber <=5:           #while 只要条件满足,就会重复执行后面的语句
+    print(current_nomber)
+    current_nomber += 1
+    
+print("----------------------------------------------------------------------------------------")
+#7.2.2 让用户选择何时退出
+print("7.2.2 让用户选择何时退出")
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+message = ''
+while message != 'quit':
+    message = input(prompt)
+    if message != 'quit':
+        print(message)
+
+print("----------------------------------------------------------------------------------------")
+#7.2.3使用标志
+print("7.2.3使用标志")
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+active = True                       #将active作为结束的标志 
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
+
+print("----------------------------------------------------------------------------------------")
+#7.2.使用break退出循环
+print("7.2.4使用break退出循环")
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+while True:
+    city = input(prompt)
+    if city == 'quit':
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
+        
+print("----------------------------------------------------------------------------------------")
+#7.2.5 在循环中使用 continue
+print("7.2.5 在循环中使用 continue")
+current_nomber = 0
+while current_nomber < 10:
+    current_nomber +=1
+    if current_nomber % 2==0:   
+        continue                            #当执行continue时程序会立即退出该循环,并进行下一次的循环,continue后面的语句将不会被执行
+    print(current_nomber)
+    
+print("----------------------------------------------------------------------------------------")
+#7.2.6 避免无限循环
+print("7.2.6 避免无限循环")
+x = 1
+while x<=5:
+    print(x)                                 #当程序被写成了无限循环时,可以按下ctrl+c键退出程序
--- priactise1.8.py
+++ priactise1.8.py
@@ -13,10 +13,61 @@ else:
     print("There have some free table")
     
 print("----------------------------------------------------------------------------------------")
-#7.2
+#7.3
 nomber = input("Please tell me a nomber")
 nomber = int(nomber)
 if nomber % 10 == 0:
     print("Yes!")
 else:
     print("No!")
+
+print("----------------------------------------------------------------------------------------")
+#7.4
+while True:
+    pizza = input('Please tell what do you want')
+    if pizza == 'quit':
+        break
+    else:
+        print(f"{pizza} will be add to your pizza!")
+        
+print("----------------------------------------------------------------------------------------")
+#7.5
+while True:
+    age= input("How old are yuo?")
+    try:
+        age =int(age)
+    except:
+        break
+    if age <=3:
+        print("you are free!")
+    elif age <=12:
+        print("you need to pay 10$.")
+    else :
+        print("you need to pay 15$.")
+
+print("----------------------------------------------------------------------------------------")
+#7.6
+pizza = ''
+while pizza != 'quit':
+    pizza = input('Please tell what do you want')
+    print(f"{pizza} will be add to your pizza!")
+
+active =True
+while active:
+    pizza = input('Please tell what do you want')
+    if pizza == 'quit':
+        active = False
+    else:
+        print(f"{pizza} will be add to your pizza!")
+
+while True:
+    pizza = input('Please tell what do you want')
+    if pizza == 'quit':
+        break
+    else:
+        print(f"{pizza} will be add to your pizza!")
+
+print("----------------------------------------------------------------------------------------")
+#7.7
+while 1:
+    print("Unlimit time")
