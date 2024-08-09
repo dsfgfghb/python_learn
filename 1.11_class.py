38a94a82c73b60deaf10811607a4f63675239be4
fjy <q13467900@outlook.com>
2024-08-09 10:27:31 +0800

    1.11_class

--- 1.11_class.py
+++ 1.11_class.py
@@ -28,3 +28,77 @@ your_dog = Dog('Lucy',3)
 print(f"your dog's name is {your_dog.name}.")           #创建多个类
 print(f"your dog is {your_dog.age} years old.")
 your_dog.sit()
+
+print("----------------------------------------------------------------------------------------")
+#9.2.1使用类和实例 
+print("9.2.1使用类和实例")
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
+
+print("----------------------------------------------------------------------------------------")
+#9.2.2给属性指定默认值 
+print("9.2.2给属性指定默认值")
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+
+print("----------------------------------------------------------------------------------------")
+#9.2.3 修改属性的值 
+print("9.2.3 修改属性的值")
+
+my_new_car.odometer_reading = 23            #直接通过实例访问
+my_new_car.read_odometer()
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
+    def update_odometer(self,mileage):                              #通过方法修改
+        if mileage>=self.odometer_reading:
+            self.odometer_reading=mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self,miles):                         #通过方法令该值递增
+        self.odometer_reading+=miles
+    
+my_new_car = Car('audi', 'a4', 2024)
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+
+my_used_car=Car('subaru','outback',2019)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
--- priactise1.11.py
+++ priactise1.11.py
@@ -4,6 +4,7 @@ class Restaurant:
     def __init__(self,name,type):
         self.restaurant_name=name
         self.cuisine_type=type
+        self.number_served = 0
     
     def describe_restaurant(self):
         print(f"Restaurant 's name is {self.restaurant_name}")
@@ -11,6 +12,14 @@ class Restaurant:
         
     def open_restaurant(self):
         print("The Restaurant is open")
+    
+    def set_number_served(self,number):
+        self.number_served=number
+    
+    def  increment_number_served(self,number):
+        self.number_served+=number
+
+        
     
 a=Restaurant('sfoods','big')
 print(f'name: {a.restaurant_name} cuisine_type: {a.cuisine_type}')
@@ -33,13 +42,43 @@ class User:
     def __init__(self,first_name,last_name):
         self.first_name=first_name
         self.last_name=last_name
+        self.login_attempts = 0
         
     def describe_user(self):
         print(f"first name:{self.first_name} , last name: {self.last_name}")
 
     def greet_user(self):
         print(f"Hello! {self.first_name} {self.last_name}")
+    
+    def increment_login_attempts(self):
+        self.login_attempts+=1
+    
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
 
 you = User('tom','sarar')
 you.describe_user()
 you.greet_user()
+
+print("----------------------------------------------------------------------------------------")
+#9.4
+restaurant = Restaurant('Abc','big')
+restaurant.number_served = 10
+print(restaurant.number_served)
+
+restaurant.set_number_served(222)
+print(restaurant.number_served)
+
+restaurant.increment_number_served(30)
+print(restaurant.number_served)
+
+print("----------------------------------------------------------------------------------------")
+#9.5
+Tom = User('Tom','sarae')
+print(Tom.login_attempts)
+for i in range(10):
+    Tom.increment_login_attempts()
+print(Tom.login_attempts)
+Tom.reset_login_attempts()
+print(Tom.login_attempts)
