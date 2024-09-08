edac3032b30facdf6d54031a5a4aa3176c743c40
fjy <q13467900@outlook.com>
2024-09-08 23:43:40 +0800

    admin

--- learning_log/learning_logs/admin.py
+++ learning_log/learning_logs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Topic   #前面的 . 是让Django在adimin.py这个文件查找models.py
+from .models import Entry
+admin.site.register(Topic)
+admin.site.register(Entry)
--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -9,3 +9,13 @@ class Topic(models.Model):              # 一个继承了model的类
 
     def __str__(self):   #如果模型有__str__()方法,那么每次需要生成表示模型实例的输出时,都会调用这个方法
         return self.text
+    
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)#实参 on_delete=models.CASCADE 让Django 在删除主题的同时删除所有与之相关联的条目，这称为级联删除
+    text = models.TextField()   # TextField实例 长度不受限
+    date_added = models.DateTimeField(auto_now_add=True)#让我们能够按创建顺序呈现条目，并在每个条目旁边放置时间戳。
+
+    class Meta:
+        verbose_name_plural = 'entries'
+    def __str__(self):
+        return f"{self.text[:50]}..."
