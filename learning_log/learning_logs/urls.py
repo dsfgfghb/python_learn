ac69d2d96f435b4cd57350bb4e43229249dd1795
fjy <q13467900@outlook.com>
2024-09-19 23:00:58 +0800

    topic&entry

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     # 主页
     path('', views.index, name='index'),
     path('topics/',views.topics,name='topics'),
-    path('topics/<int:topic_id>/', views.topic, name='topic')
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
+    path('new_entry/<int:topic_id>',views.new_entry,name='new_entry'),
+    path('edit_entry/<int:entry_id>',views.edit_entry,name='edit_entry'),
 ]   
 #     让 Django 查找在基础 URL 后紧跟单词 topics 的
 # URL，第二部分（/<int:topic_id>/）与在两个斜杠之间的整
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Topic
+from django.shortcuts import render,redirect
+from .models import Topic,Entry
+from .forms import TopicForm,EntryForm
 
 # Create your views here.
 def index(request):
@@ -15,3 +16,45 @@ def topic(request,topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic,'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()          #如果为GET 请求，创建一个新表单
+        print("ijk")
+    else:
+        form = TopicForm(data = request.POST)
+        if form.is_vaild():#方法 is_valid() 核实用户填写了所有必不可少的字段
+            
+            form.save()#调用save()，将表单中的数据写入数据库
+            return redirect('learning_logs:topics')#使用 redirect()将用户的浏览器重定向到页面 topics
+    
+    context = {'form':form}
+    return render(request,'learning_logs/new_topic.html',context)
+
+def new_entry(request,topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method !='Post':
+        form = EntryForm()
+        print("ijk")
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic',topic_id=topic_id)
+    context = {'topic':topic,'form':form}
+    return render(request,'learning_logs/new_entry.html',context)
+
+def edit_entry(request,entry_id):
+    entry=Entry.objects.get(id=entry_id)
+    topic=entry.topic
+    if request.method!='POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry,data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic',topic_id=topic.id)
+    context = {'entry':entry,'topic':topic,'form':form}
+    return render(request,'learning:logs/endit_entry.html',context)
