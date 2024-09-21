e45e593602b8ee05fa70ee44b71de8cb8fad3094
fjy <q13467900@outlook.com>
2024-09-21 22:14:28 +0800

    edit_entry

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -20,11 +20,9 @@ def topic(request,topic_id):
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()          #如果为GET 请求，创建一个新表单
-        print("ijk")
     else:
         form = TopicForm(data = request.POST)
-        if form.is_vaild():#方法 is_valid() 核实用户填写了所有必不可少的字段
-            
+        if form.is_valid():#方法 is_valid() 核实用户填写了所有必不可少的字段
             form.save()#调用save()，将表单中的数据写入数据库
             return redirect('learning_logs:topics')#使用 redirect()将用户的浏览器重定向到页面 topics
     
@@ -33,9 +31,9 @@ def new_topic(request):
 
 def new_entry(request,topic_id):
     topic = Topic.objects.get(id=topic_id)
-    if request.method !='Post':
+    if request.method != 'POST':
         form = EntryForm()
-        print("ijk")
+        print("id")
     else:
         form = EntryForm(data=request.POST)
         if form.is_valid():
@@ -47,14 +45,15 @@ def new_entry(request,topic_id):
     return render(request,'learning_logs/new_entry.html',context)
 
 def edit_entry(request,entry_id):
+    print("edit_entry")
     entry=Entry.objects.get(id=entry_id)
     topic=entry.topic
-    if request.method!='POST':
-        form = EntryForm(instance=entry)
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)#使用实参 instance=entry 创建一个EntryForm 实例
     else:
         form = EntryForm(instance=entry,data=request.POST)
         if form.is_valid():
             form.save()
             return redirect('learning_logs:topic',topic_id=topic.id)
     context = {'entry':entry,'topic':topic,'form':form}
-    return render(request,'learning:logs/endit_entry.html',context)
+    return render(request,'learning_logs/edit_entry.html',context)
